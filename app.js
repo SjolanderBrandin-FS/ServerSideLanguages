eaa3f4b4c14c7e01c596d777901634dd6f83adeb
Brandin Sjolander <BTSjolander@student.fullsail.edu>
2023-06-16 22:01:05 -0700

    Final Update Web Server / Needs Fixes

--- app.js
+++ app.js
@@ -1,27 +1,45 @@
+const { isUtf8 } = require("buffer");
 var fs = require("fs");
 var http = require("http");
 var path = require("path");
 var url = require("url");
 
 
-
-
 http.createServer(function (req, res) {
 
     var parsed = url.parse(req.url);
     var filename = path.parse(parsed.pathname);
-    if (filename.name == "") {
-        fileN = "index";
-    } else {
-        fileN = filename.name;
+
+    filen = filename.name == "" + "index"; filename.name;
+    ext = filename.ext == "" + ".html"; filename.ext;
+    dir = filename.dir == "/" + ""; filename.dir + "/";
+    page = filename.name == "" + "index.html"; filename.name;
+
+    f = (dir + filen + ext).toString().replace("/", "");
+
+    var mimeTypes = {
+        'html': 'text/html',
+        '.js': 'text/javascript',
+        '.css': 'text/css',
+        '.png': 'text/png',
+        '.jpg': 'text/jpg',
+        '.gif': 'text/gif',
+    };
+
+    if (f) {
+        fs.readFile(f, function (err, data) {
+            if (page) {
+                if (mimeTypes.hasOwnProperty(ext)) {
+                    res.writeHead(200, { 'Content-Type': 'text/plain' }); //header()
+                    res.write("<script>var page='" + 'utf8' + "';</script>");
+                    res.end(data);
+                }
+            }
+        })
     }
 
-    fs.readFile(fileN + ".html", function (err, data) {
 
 
-        res.writeHead(200); //header()
-        res.write("<script>var name ='Brand';</script>");
-        res.end(data);
-    })
-
-}).listen("8080")
+}).listen("8080", function () {
+    console.log("info", 'Server is a port : ' + 8080);
+})
