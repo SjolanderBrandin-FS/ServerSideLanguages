8dd1f512eb533638b815956b7741e5787dd83043
Brandin Sjolander <BTSjolander@student.fullsail.edu>
2023-06-24 18:27:47 -0700

    Feedback Update Unsuccessful

--- app.js
+++ app.js
@@ -10,12 +10,15 @@ http.createServer(function (req, res) {
     var parsed = url.parse(req.url);
     var filename = path.parse(parsed.pathname);
 
-    filen = filename.name == "" + "index"; filename.name;
-    ext = filename.ext == "" + ".html"; filename.ext;
+    filen = filename.name || "" + "index"; filename.name;
+    ext = filename.ext || "" + ".html"; filename.ext;
     dir = filename.dir == "/" + ""; filename.dir + "/";
-    page = filename.name == "" + "index.html"; filename.name;
+    page = filename.name || "" + "index.html"; filename.name;
+
+    console.log(">>>>>>>>>>>>>>>>>>> ", dir, filen, ext);
 
     f = (dir + filen + ext).toString().replace("/", "");
+
 
     var mimeTypes = {
         'html': 'text/html',
@@ -28,12 +31,12 @@ http.createServer(function (req, res) {
 
     if (f) {
         fs.readFile(f, function (err, data) {
+            //console.log(err);
             if (page) {
-                console.log(err)
                 if (mimeTypes.hasOwnProperty(ext)) {
                     res.writeHead(200, { 'Content-Type': 'text/html' }); //header()
                     res.write("<script>var page=" + 'utf8' + ";</script>");
-                    res.write("<script>var page=" + filename + ";</srcipt>");
+                    res.write("<script>var page=" + f + ";</srcipt>");
                     res.end(data);
 
                 }
@@ -43,6 +46,7 @@ http.createServer(function (req, res) {
 
 
 
-}).listen("8080", function () {
+}).listen("8080", function (err) {
     console.log("info", 'Server is a port : ' + 8080);
+    console.log(err);
 })
